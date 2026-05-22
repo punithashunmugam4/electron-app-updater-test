799d8725468067a702d54856762f9a7f4f8cfd1a
punithashunmugam4 <punithashunmugam4@gmail.com>
2026-05-23 00:40:12 +0530

    version 1.0.3

--- downloads.cjs
+++ downloads.cjs
@@ -1,5 +1,5 @@
 // const { ipcRenderer } = require("electron");
-// version 1.0.2
+// version 1.0.3
 const showDownloadModal = () => {
   const modal = document.getElementById("download-modal");
   modal.style.display = "block";
